562b20516d50eb3bc75200f46ff3a7f88d5b48ce
chocopieo <ysj1106@gmail.com>
2024-11-26 15:05:25 +0900

    add event

---
+++ vue/project/src/views/2_event/EventChangeView.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <select name="" id="" @change="changeCity($event)" v-model="selectedCity">
+      <option value="">== 도시선택 ==</option>
+      <option
+        :value="city.cityCode"
+        :key="city.cityCode"
+        v-for="city in cityList"
+      >
+        {{ city.title }}
+      </option>
+    </select>
+    <select name="" id="">
+      <option value="">== 동선택 ==</option>
+      <option
+        :value="dong.dongCode"
+        :key="dong.dongCode"
+        v-for="dong in selectedDongList"
+      >
+        {{ dong.dongTitle }}
+      </option>
+    </select>
+    <!-- <select name="" id="">
+      <option value="">== 동선택 ==</option>
+      <option
+        :value="dong.dongCode"
+        :key="dong.dongCode"
+        v-for="dong in dongList.filter(
+          (dong) => dong.cityCode === selectedCity
+        )"
+      >
+        {{ dong.dongTitle }}
+      </option>
+    </select> -->
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      selectedCity: '',
+      cityList: [
+        { cityCode: '02', title: '서울' },
+        { cityCode: '051', title: '부산' },
+        { cityCode: '064', title: '제주' }
+      ],
+      dongList: [
+        { cityCode: '02', dongCode: '1', dongTitle: '중구' },
+        { cityCode: '02', dongCode: '2', dongTitle: '강남구' },
+        { cityCode: '02', dongCode: '3', dongTitle: '강서구' },
+        { cityCode: '02', dongCode: '4', dongTitle: '강동구' },
+        { cityCode: '02', dongCode: '5', dongTitle: '강북구' },
+        { cityCode: '051', dongCode: '1', dongTitle: '금정구' },
+        { cityCode: '051', dongCode: '2', dongTitle: '해운대구' },
+        { cityCode: '051', dongCode: '3', dongTitle: '연제구' },
+        { cityCode: '051', dongCode: '4', dongTitle: '동래구' },
+        { cityCode: '064', dongCode: '1', dongTitle: '서귀포동' },
+        { cityCode: '064', dongCode: '2', dongTitle: '제주동' }
+      ],
+      selectedDongList: []
+    }
+  },
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    changeCity(evt) {
+      console.log(evt.target.tagName)
+      this.selectedDongList = this.dongList.filter(
+        (dong) => dong.cityCode === this.selectedCity
+      )
+    }
+  }
+}
+</script>
---
+++ vue/project/src/views/2_event/EventKeyView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <!-- <input
+      type="search"
+      name=""
+      id=""
+      @keyup="checkEnter($event)"
+      v-model="searhText"
+    /> -->
+    <input
+      type="search"
+      name=""
+      id=""
+      @keyup.enter="doSearch"
+      v-model="searhText"
+    />
+    <button @click="doSearch">조회</button>
+    <button type="submit" @click.prevent="doSearch"></button>
+  </div>
+</template>
+<script>
+// .enter
+// .tab
+// .delete
+// .esc
+// .space
+// .up
+// .down
+// .left
+// .right
+// .stop - event.stopPropagation()
+// .prevent - event.preventDefault()
+export default {
+  components: {},
+  data() {
+    return {
+      searhText: ''
+    }
+  },
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    doSearch() {
+      console.log(this.searhText)
+    },
+    checkEnter(e) {
+      if (e.keyCode === 13) {
+        this.doSearch()
+      }
+    }
+  }
+}
+</script>

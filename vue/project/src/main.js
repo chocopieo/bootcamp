221efcc76b697c6144b24a94d520bbc10e4c8c11
chocopieo <ysj1106@gmail.com>
2024-11-29 17:10:01 +0900

    add bootstrap

--- vue/project/src/main.js
+++ vue/project/src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import store from './store'
 import PageTitle from './components/fragments/PageTitle.vue'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
 const app = createApp(App)
 app.use(store)
 app.use(router)
--- vue/project/src/views/4_reuse/PageView.vue
+++ vue/project/src/views/4_reuse/PageView.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>
-    <PageTitle title="고객목록" />
+  <div class="container">
+    <page-title title="고객목록" />
     <h3 style="border-left: 5px solid red; padding-left: 3px; text-align: left">
       고객목록
     </h3>
+    <button class="btn btn-danger">클릭</button>
   </div>
 </template>
 <script>

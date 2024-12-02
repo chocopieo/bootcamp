0ca1c91c955ed8d40caa5a8ee05e6264d3dde745
chocopieo <ysj1106@gmail.com>
2024-12-02 18:01:53 +0900

    add reuse component (grid)

--- vue/project/src/router/index.js
+++ vue/project/src/router/index.js
@@ -175,6 +175,14 @@ const routes = [
       import(
         /* webpackChunkName: "resue", webpackPrefetch: true */ '../views/4_reuse/ParentView.vue'
       )
+  },
+  {
+    path: '/reuse/list',
+    name: 'ListView',
+    component: () =>
+      import(
+        /* webpackChunkName: "resue", webpackPrefetch: true */ '../views/4_reuse/ListView.vue'
+      )
   }
 ]
 
